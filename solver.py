a77970dea3d46bd82988ae67f2935296c576e34d
Jack A. Botelho <35936122+jbotelho97@users.noreply.github.com>
2019-03-06 09:39:44 -0500

    Add files via upload

---
+++ solver.py
@@ -0,0 +1,20 @@
+import rubik
+import heapq
+
+
+def shortest_path(start, end):
+    """
+    Using 2-way BFS, finds the shortest path from start_position to
+    end_position. Returns a list of moves. 
+
+    You can use the rubik.quarter_twists move set.
+    Each move can be applied using rubik.perm_apply
+    """
+
+    startQ = [start]
+    endQ = [end]
+
+
+
+    raise NotImplementedError
+
